4ef62db80956494e887fe63a340c6a78a0f7a5aa
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-12-11 07:46:43 +0900

    Add: Builder main function (#18)

---
+++ test/all.test.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { buildNutsAPISchema } from '@src/index';
+
+it('all', async () => {
+  expect(await buildNutsAPISchema(
+    {
+      schemaRoot: resolve('./test/mocks/parseEndpoint'),
+    },
+  )).toBe([
+    'const apiSchema = {\'/deleteUsers\':{\'POST\':{request:zod.object({_id:zod.string()}),response:{200: zod.record(zod.string(),zod.never()),503: zod.object({reason:zod.literal(\'a\').or(zod.literal(\'b\')).or(zod.literal(\'c\'))})}}},\'/getUsers\':{\'POST\':{request:zod.object({_id:zod.string()}),response:{200: zod.object({user:zod.object({username:zod.string(),email:zod.string()}),empty:zod.record(zod.string(),zod.never())}),404: zod.record(zod.string(),zod.never())}}}} as const;',
+    'type ApiSchemaType = typeof apiSchema;',
+  ].join('\n'));
+});
