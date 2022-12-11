8b9ffae9604115dec90206d621c9f7f13beb98c9
AsPulse <aspulse.x@gmail.com>
2022-12-11 21:56:37 +0900

    Fix: no-change alias returning

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ import type { CustomPath } from './parseEndpoint';
 export function atSignAilas(alias: string, route: string): CustomPath {
   return (v: string) => {
     const str = v.replace(new RegExp(`^@${alias}/`), '');
-    if(str === alias) return alias;
+    if(str === v) return v;
     return resolve(route, str);
   };
 }
